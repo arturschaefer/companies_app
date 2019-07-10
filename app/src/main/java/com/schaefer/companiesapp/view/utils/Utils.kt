d4084796b8d64205151ff28954bbd12da895a3ab
Artur Schaefer <artur.schaefer@hotmail.com>
2019-07-10 03:18:24 -0300

    feat: add homeActivity and detailsActivity to val

--- app/src/main/java/com/schaefer/companiesapp/view/utils/Utils.kt
+++ app/src/main/java/com/schaefer/companiesapp/view/utils/Utils.kt
@@ -2,9 +2,12 @@ package com.schaefer.companiesapp.view.utils
 
 import android.app.Activity
 import com.schaefer.companiesapp.CompaniesApp
-import com.schaefer.companiesapp.di.component.DaggerLoginComponent
-import com.schaefer.companiesapp.di.component.LoginComponent
+import com.schaefer.companiesapp.di.component.*
+import com.schaefer.companiesapp.di.module.EnterpriseDetailModule
+import com.schaefer.companiesapp.di.module.HomeModule
 import com.schaefer.companiesapp.di.module.LoginModule
+import com.schaefer.companiesapp.view.EnterpriseDetailActivity
+import com.schaefer.companiesapp.view.HomeActivity
 import com.schaefer.companiesapp.view.LoginActivity
 
 val Activity.applicationComponent get() = (application as CompaniesApp).getApplicationComponent()
@@ -13,4 +16,16 @@ val LoginActivity.loginActivityComponent: LoginComponent
         .builder()
         .applicationComponent(applicationComponent)
         .loginModule(LoginModule(this))
+        .build()
+val HomeActivity.homeActivityComponent: HomeComponent
+    get() = DaggerHomeComponent
+        .builder()
+        .applicationComponent(applicationComponent)
+        .homeModule(HomeModule(this))
+        .build()
+val EnterpriseDetailActivity.enterpriseDetailComponent: EnterpriseDetailComponent
+    get() = DaggerEnterpriseDetailComponent
+        .builder()
+        .applicationComponent(applicationComponent)
+        .enterpriseDetailModule(EnterpriseDetailModule(this))
         .build()
