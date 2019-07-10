91e497b1b61818cc4e1cc555c78e5bbce11f1f55
Artur Schaefer <artur.schaefer@hotmail.com>
2019-07-10 03:15:44 -0300

    feat: add route to the detailsActivity and put some constants

--- app/src/main/java/com/schaefer/companiesapp/router/Router.kt
+++ app/src/main/java/com/schaefer/companiesapp/router/Router.kt
@@ -4,9 +4,11 @@ import android.app.Activity
 import android.content.Intent
 import com.schaefer.companiesapp.entity.enterprise.Enterprise
 import com.schaefer.companiesapp.entity.login.LoginResultEntity
+import com.schaefer.companiesapp.view.EnterpriseDetailActivity
 import com.schaefer.companiesapp.view.HomeActivity
 
-private const val EXTRA_LOGIN_RESULT = "initialPhrase"
+private const val EXTRA_LOGIN_RESULT = "loginResult"
+private val EXTRA_ENTEPRISE_DETAIL = "enterprise_detail"
 
 fun goToHomeScreen(view: Activity, loginEntityResult: LoginResultEntity) {
     val intent = Intent(view, HomeActivity::class.java)
@@ -15,5 +17,7 @@ fun goToHomeScreen(view: Activity, loginEntityResult: LoginResultEntity) {
 }
 
 fun goToDetailScreen(view: Activity, enterprise: Enterprise) {
-
+    val intent = Intent(view, EnterpriseDetailActivity::class.java)
+    intent.putExtra(EXTRA_ENTEPRISE_DETAIL, enterprise)
+    view.startActivity(intent)
 }
